984e688baec6ae771e7a6a4b21f0a8e98f3127db
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-24 21:22:52 +0500

    Base setting in tailwind config and delete default vue styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  plugins: [
+    require("tailwindcss-text-rendering"),
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        base: ['"Montserrat"', 'sans-serif'],
+      },
+      screens: {
+        sm: '568px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        xxl: '1440px',
+      },
+      colors: {
+        transparent: 'transparent',
+        white: {
+          base: '#FFF',
+          soft: '#CFD1DE'
+        },
+        black: {
+          base: '#000',
+          soft: '#262626'
+        },
+      },
+      backgroundImage: {
+        'gradient-dark': 'linear-gradient(90deg, #494949, #000000)'
+      }
+    },
   },
-  plugins: [],
 }
 
