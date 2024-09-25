95c223de7d97aa70485fe90548793c7ba43e98e7
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-25 17:56:32 +0500

    Create product card

---
+++ src/components/blocks/product/product.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Button from '@/components/ui/button/button.vue'
+import Icon from '@/components/ui/icon/icon.vue'
+import Image from '@/components/ui/image/image.vue'
+
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="flex flex-col rounded overflow-hidden bg-gradient-dark">
+    <div class="shrink-0 w-full h-60 bg-white-base p-4">
+      <Image class="block w-full h-full" :src="data.image" :alt="data.title" />
+    </div>
+    <div class="flex flex-col h-full gap-4 px-4 py-2">
+      <div class="">
+        {{ data.title }}
+      </div>
+      <div class="flex items-center justify-between text-xl mt-auto">
+        {{ data.price }} ₽
+        <Button type="red" size="small">
+          <Icon name="add-shopping-cart" size="normal"/>
+        </Button>
+      </div>
+    </div>
+  </div>
+</template>
