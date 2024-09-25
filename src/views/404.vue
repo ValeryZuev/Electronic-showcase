5a17c20b6f416e3ae65123c61aed9e5e69711bf0
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-25 12:58:27 +0500

    Update 404.vue

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,17 +1,24 @@
 <script setup>
-import Link from '@/components/ui/link.vue'
-import pages from '@/config/project/pages.js'
+import Image from '@/components/ui/image/image.vue'
+import Link from '@/components/ui/link/link.vue'
+import { pages, images } from '@/config/project/index.js'
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center gap-16 h-full m-auto">
-    <h1 class="flex items-center gap-4 text-[50px] text-light-100">
+    <Link :href="pages.home">
+      <Image
+        class="max-w-80 block"
+        :src="images.logo"
+        alt="RGD" />
+    </Link>
+    <h1 class="flex items-center gap-4 text-[50px]">
       <span class="font-bold border-r pr-10 mr-8">404</span>
       <span class="text-2xl">Страница не найдена</span>
     </h1>
     <Link
       :href="pages.home"
-      type="green-rose"
+      type="red-outline"
       size="middle">
       На главную
     </Link>
