c3e4f761a0c366e3a91a32b44c042bf6a0aed1db
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-25 12:56:19 +0500

    Create ui component Icon

---
+++ src/components/ui/icon/icon.vue
@@ -0,0 +1,48 @@
+<script setup>
+import Components from '@/components/ui/icon/icons-components.js'
+import { setBasicUiProps } from '@/compositions/ui/setBasicUiProps.js'
+
+const props = defineProps({
+  name: {
+    type: String,
+    default: '',
+  },
+  size: {
+    type: String,
+    default: 'normal',
+  },
+})
+
+const { classes } = setBasicUiProps(props, 'icon-component')
+</script>
+
+<template>
+  <div :class="classes">
+    <component
+      class="block w-full h-full"
+      :is="Components[name]" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.icon-component {
+  @apply shrink-0;
+  &.size {
+    &-micro {
+      @apply w-2.5 h-2.5;
+    }
+    &-small {
+      @apply w-3 h-3;
+    }
+    &-normal {
+      @apply w-4 h-4;
+    }
+    &-middle {
+      @apply w-6 h-6;
+    }
+    &-large {
+      @apply w-8 h-8;
+    }
+  }
+}
+</style>
