7e9a7155eb8ea3fc7bf79a2af435b50da6f9f0de
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-25 16:55:15 +0500

    Added icon chevron-down

---
+++ src/components/icons/arrows/chevron-down.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- eslint-disable -->
+  <svg
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg">
+    <path
+      d="M12 14.3338L21.3338 5L24 7.66625L12 19.6662L0 7.66625L2.66625 5L12 14.3338Z"
+      fill="currentColor" />
+  </svg>
+</template>
--- src/components/ui/icon/icons-components.js
+++ src/components/ui/icon/icons-components.js
@@ -1,5 +1,7 @@
+import ChevronDown from '@/components/icons/arrows/chevron-down.vue'
 import ShoppingCart from '@/components/icons/shopping-cart.vue'
 
 export default {
+  'chevron-down': ChevronDown,
   'shopping-cart': ShoppingCart,
 }
