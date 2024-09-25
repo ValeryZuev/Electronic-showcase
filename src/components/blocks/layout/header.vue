391dfa010d2ffe00dc926f1ee24b0b6678021669
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-25 12:58:34 +0500

    Create header.vue

---
+++ src/components/blocks/layout/header.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Icon from '@/components/ui/icon/icon.vue'
+import Image from '@/components/ui/image/image.vue'
+import Link from '@/components/ui/link/link.vue'
+import PillBadge from '@/components/ui/pill-badge/pill-badge.vue'
+import { pages, images } from '@/config/project/index.js'
+
+import { useBasket } from '@/stores/basket.js'
+
+const { items } = useBasket()
+</script>
+
+<template>
+  <header>
+    <Link :href="pages.home">
+      <Image
+        class="max-w-60 block"
+        :src="images.logo"
+        alt="RGD" />
+    </Link>
+    <div class="flex-1">
+      <slot />
+    </div>
+    <Link class="relative" :href="pages.basket" type="secondary">
+      <Icon name="shopping-cart" size="middle"/>
+      <PillBadge
+        v-if="items.length"
+        size="normal">
+        {{ items.length }}
+      </PillBadge>
+    </Link>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  @apply flex items-center justify-between gap-4 bg-gradient-dark py-4 px-8 rounded;
+}
+</style>
