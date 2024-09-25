c8b36f68aa68cb213c6e0e0ed3ffa1b7baf1b86a
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-25 13:11:15 +0500

    Create footer.vue

---
+++ src/components/blocks/layout/footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer>
+    <slot />
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+  @apply flex items-center justify-between gap-4 text-xs text-white-soft bg-gradient-dark py-4 px-8 rounded-t;
+}
+</style>
