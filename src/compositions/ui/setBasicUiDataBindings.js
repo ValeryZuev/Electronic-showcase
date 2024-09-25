d66e200f4b6c30b7e150751181c6e86e11aa6dfb
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-25 10:27:39 +0500

    Create compositions ui props

---
+++ src/compositions/ui/setBasicUiDataBindings.js
@@ -0,0 +1,34 @@
+export const setBasicUiDataBindings = (emit) => {
+  const MIN_ARRAY_LENGTH = 0
+
+  const change = (e) => {
+    if (e.target) {
+      const { value, checked, type } = e.target
+
+      let model = ['text', 'number', 'range', 'textarea', 'password']
+        .includes(type) ? value : value || 'undefined'
+      if (checked !== 'undefined' && type === 'checkbox') {
+        model = checked
+      }
+      emit('update:modelValue', model)
+      emit('change', model)
+
+      return value
+    }
+    if (['string', 'number', 'boolean'].includes(typeof e)
+        || Object.prototype.hasOwnProperty.call(e, 'value')
+        || Array.isArray(e)) {
+      emit('update:modelValue', e)
+      emit('change', e)
+      return e
+    }
+    if (e.length >= MIN_ARRAY_LENGTH) {
+      emit('update:modelValue', e)
+      emit('change', e)
+      return e
+    }
+    return e
+  }
+
+  return { change }
+}
---
+++ src/compositions/ui/setBasicUiProps.js
@@ -0,0 +1,48 @@
+import { computed } from 'vue'
+
+const TYPES = {
+  primary: 'primary',
+  secondary: 'secondary',
+  'green-rose': 'green-rose'
+}
+
+const SIZES = {
+  micro: 'micro',
+  small: 'small',
+  normal: 'normal',
+  middle: 'middle',
+  large: 'large'
+}
+
+const paramsAsJoinedString = (name, value) => {
+  let string = ` ${name}`
+  if (value) { string += `-${value}` }
+  return string
+}
+
+export const setBasicUiProps = (props, componentClass) => {
+  const classes = computed(() => {
+    let classesAsString = componentClass || ''
+    if (props.type && TYPES[props.type]) {
+      classesAsString += paramsAsJoinedString('type', props.type)
+    }
+    if (props.size && SIZES[props.size]) {
+      classesAsString += paramsAsJoinedString('size', props.size)
+    }
+    if (props.readonly) {
+      classesAsString += paramsAsJoinedString('state-readonly')
+    }
+    if (props.disabled) {
+      classesAsString += paramsAsJoinedString('state-disabled')
+    }
+    if (props.error) { classesAsString += paramsAsJoinedString('state-error') }
+    if (props.classes) {
+      classesAsString += paramsAsJoinedString(props.classes)
+    }
+    return classesAsString
+  })
+
+  return {
+    classes
+  }
+}
