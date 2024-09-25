ac94f0dc9a28f29c3b27b26a95af23657d74ce35
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-25 13:11:28 +0500

    Create default layout

---
+++ src/layouts/default.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Header from '@/components/blocks/layout/header.vue'
+import Footer from '@/components/blocks/layout/footer.vue'
+</script>
+
+<template>
+  <div class="container flex flex-col pt-8 flex-1">
+    <Header />
+    <main></main>
+    <Footer>
+      &#169; {{ new Date().getFullYear() }} Все права защищены.
+    </Footer>
+  </div>
+</template>
