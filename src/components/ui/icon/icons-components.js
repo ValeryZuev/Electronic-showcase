7b1511e68b8f0845ec3d9d810caa844d79b9da5f
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-25 17:56:50 +0500

    Add icon add-shopping-cart

--- src/components/ui/icon/icons-components.js
+++ src/components/ui/icon/icons-components.js
@@ -1,7 +1,9 @@
 import ChevronDown from '@/components/icons/arrows/chevron-down.vue'
+import AddShoppingCart from '@/components/icons/add-shopping-cart.vue'
 import ShoppingCart from '@/components/icons/shopping-cart.vue'
 
 export default {
   'chevron-down': ChevronDown,
+  'add-shopping-cart': AddShoppingCart,
   'shopping-cart': ShoppingCart,
 }
