b8a1dea9fb1cb05f177a543a2d23ad477b808d4b
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-27 15:32:30 +0500

    Create item loader

---
+++ src/components/ui/loader/loader.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { setBasicUiProps } from '@/compositions/ui/setBasicUiProps.js'
+const props = defineProps({
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+})
+const { classes } = setBasicUiProps(props, 'loader-component')
+</script>
+
+<template>
+  <div
+    v-if="loading"
+    :class="classes" />
+  <slot v-else />
+</template>
+
+<style lang="scss" scoped>
+.loader-component {
+  @apply relative bg-gradient-dark min-h-[300px] rounded-md overflow-hidden;
+  &::before {
+    content: '';
+    @apply absolute top-0 left-1/2 h-full w-6 bg-white-soft blur-2xl -skew-x-12;
+    animation: 3s infinite running;
+  }
+}
+
+@keyframes running {
+  from {
+    @apply -left-1/4;
+  }
+  to {
+    @apply left-full;
+  }
+}
+</style>
