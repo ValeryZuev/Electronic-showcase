3908dde0ce42891c7f318ddd235ca77d46ccdd50
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-27 21:26:00 +0500

    Change basket store

--- src/stores/basket.js
+++ src/stores/basket.js
@@ -1,8 +1,60 @@
 import { defineStore } from 'pinia'
+import db from '@/core/db/db'
 
 export const useBasket = defineStore('basket', {
   state: () => ({
-    items: []
+    items: db.get('rgd-basket') ? JSON.parse(db.get('rgd-basket')) : {}
   }),
-  actions: {}
+  getters: {
+    sumOfCounts(state) {
+      const output = {
+        count: Object.values(state.items).reduce((sum, item) => {
+          return sum + item.count
+        }, 0),
+        sum: Object.values(state.items).reduce((sum, item) => {
+          return sum + item.price * item.count
+        }, 0)
+      }
+      return output
+    }
+  },
+  actions: {
+    addToCart(product) {
+      if (product.id in this.items) {
+        this.items[product.id].count += 1
+      } else {
+        this.items[product.id] = {
+          ...product,
+          count: 1
+        }
+      }
+      this.saveStateToSession()
+    },
+    removeToCart(id) {
+      if (!(id in this.items)) return
+      delete this.items[id]
+      this.saveStateToSession()
+    },
+    changeCountItem(id, type) {
+      if (!type || !id) return
+
+      switch (type) {
+        case 'inc':
+          this.items[id].count++
+          break;
+        case 'dec':
+          if (this.items[id].count <= 1) {
+            this.removeToCart(id)
+            return
+          }
+          this.items[id].count--
+          break
+      }
+
+      this.saveStateToSession()
+    },
+    saveStateToSession() {
+      db.set('rgd-basket', JSON.stringify(this.items))
+    }
+  }
 })
