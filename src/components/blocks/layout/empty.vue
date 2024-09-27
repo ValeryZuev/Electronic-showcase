0266dee030d31d06f162c1ad280de4a30c1df905
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-27 17:43:54 +0500

    Create empty block-component

---
+++ src/components/blocks/layout/empty.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="w-full m-auto">
+    <template v-if="$slots.default">
+      <slot name="default" />
+    </template>
+    <template v-else>
+      <h2 class="text-center">По вашему запросу ничего не найдено</h2>
+    </template>
+  </section>
+</template>
