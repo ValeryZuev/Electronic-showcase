a60bb93ef4a8b4406f10c8eea1dd908dd1569135
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-27 15:31:50 +0500

    Create components input

---
+++ src/components/ui-extend/range-input/range-input.vue
@@ -0,0 +1,44 @@
+<script setup>
+import Input from '@/components/ui/input/input.vue'
+
+const props = defineProps({
+  modelValue: {
+    type: Object,
+    default: () => ({ from: 0, to: 0 })
+  },
+  min: {
+    type: Number,
+    default: 0
+  },
+  max: {
+    type: Number,
+    default: 9999
+  }
+})
+const emit = defineEmits(['update:modelValue'])
+
+const update = () => {
+  emit('update:modelValue', { ...props.modelValue })
+}
+</script>
+
+<template>
+  <div class="flex items-center gap-2">
+    <Input
+      v-model="modelValue.from"
+      native-type="number"
+      :placeholder="'От ' + min"
+      :min="min"
+      :max="max"
+      @update:model-value="update"
+    />
+    <Input
+      v-model="modelValue.to"
+      native-type="number"
+      :placeholder="'До ' + max"
+      :min="min"
+      :max="max"
+      @update:model-value="update"
+    />
+  </div>
+</template>
---
+++ src/components/ui/input/input.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { setBasicUiDataBindings } from '@/compositions/ui/setBasicUiDataBindings.js'
+import { setBasicUiProps } from '@/compositions/ui/setBasicUiProps.js'
+
+import { ref } from 'vue'
+
+const props = defineProps({
+  modelValue: {
+    type: [String, Number],
+    default: '',
+  },
+  nativeType: {
+    type: String,
+    default: 'text',
+  },
+  type: {
+    type: String,
+    default: 'primary',
+  },
+  size: {
+    type: String,
+    default: 'normal',
+  },
+  placeholder: {
+    type: String,
+    default: 'Type here...',
+  },
+  min: {
+    type: [Number, String],
+    default: 0,
+  },
+  max: {
+    type: [Number, String],
+    default: null,
+  },
+})
+
+const emit = defineEmits(['update:modelValue', 'change'])
+const { change } = setBasicUiDataBindings(emit)
+const { classes } = setBasicUiProps(props, 'input-component')
+</script>
+
+<template>
+  <input
+    :type="nativeType"
+    :value="modelValue"
+    class="w-full h-full"
+    :class="classes"
+    :placeholder="placeholder"
+    :min="min"
+    :max="max"
+    @change="change"
+    @input="change" />
+</template>
+
+<style lang="scss" scoped>
+.input-component {
+  @apply text-black-soft px-4 py-2 rounded-sm focus:outline-none;
+  &::placeholder {
+    @apply text-black-soft/80;
+  }
+  &.type {
+    &-primary {
+     @apply bg-white-soft;
+    }
+  }
+}
+</style>
