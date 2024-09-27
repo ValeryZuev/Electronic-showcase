448bd327b2489711f79605ed03eec99d06671dea
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-27 21:52:41 +0500

    Create popup order

---
+++ src/components/ui/popup/order/order.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Button from '@/components/ui/button/button.vue'
+import { usePopup } from '@/stores/popup'
+const { close } = usePopup()
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-4">
+    Ваш заказ успешно оформлен!
+    <Button
+      type="red"
+      @click="close">
+      Закрыть
+    </Button>
+  </div>
+</template>
