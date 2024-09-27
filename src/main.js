34caa46ad5cbe86b2ce83728e1ee92de855940e9
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-27 15:34:15 +0500

    Added init network in main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import './styles/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { Network } from '@/core/network.js'
+import { setApiInstance } from '@/core/api.js'
+
+import apiConfig from '@/config/env/index.js'
 import App from './App.vue'
 import router from './router'
 
@@ -10,5 +14,8 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+const api = new Network(apiConfig)
+setApiInstance(api)
 
 app.mount('#app')
