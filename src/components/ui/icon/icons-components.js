25e99ff05ea1689ce605ae40d57d20318f7d458b
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-27 21:25:34 +0500

    Added icons

--- src/components/ui/icon/icons-components.js
+++ src/components/ui/icon/icons-components.js
@@ -1,9 +1,13 @@
 import ChevronDown from '@/components/icons/arrows/chevron-down.vue'
 import AddShoppingCart from '@/components/icons/add-shopping-cart.vue'
 import ShoppingCart from '@/components/icons/shopping-cart.vue'
+import plus from '@/components/icons/plus.vue'
+import minus from '@/components/icons/minus.vue'
 
 export default {
   'chevron-down': ChevronDown,
   'add-shopping-cart': AddShoppingCart,
   'shopping-cart': ShoppingCart,
+  plus,
+  minus
 }
