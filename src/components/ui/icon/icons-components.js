676a2957c5d1cd1ff1c8e183aac2cea7affc5e59
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-27 21:52:13 +0500

    Added close icon

--- src/components/ui/icon/icons-components.js
+++ src/components/ui/icon/icons-components.js
@@ -3,11 +3,13 @@ import AddShoppingCart from '@/components/icons/add-shopping-cart.vue'
 import ShoppingCart from '@/components/icons/shopping-cart.vue'
 import plus from '@/components/icons/plus.vue'
 import minus from '@/components/icons/minus.vue'
+import close from '@/components/icons/close.vue'
 
 export default {
   'chevron-down': ChevronDown,
   'add-shopping-cart': AddShoppingCart,
   'shopping-cart': ShoppingCart,
   plus,
-  minus
+  minus,
+  close
 }
