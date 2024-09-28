7c53e2e69225a6d681e32743629d4aebda6dcf07
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-28 12:14:32 +0500

    Added protect before each router in main js

--- src/main.js
+++ src/main.js
@@ -9,6 +9,8 @@ import { setApiInstance } from '@/core/api.js'
 import apiConfig from '@/config/env/index.js'
 import App from './App.vue'
 import router from './router'
+import { pages } from '@/config/project/index.js'
+import { routerAuthController } from '@/router/middlewares/AuthController.js'
 
 const app = createApp(App)
 
@@ -17,5 +19,9 @@ app.use(router)
 
 const api = new Network(apiConfig)
 setApiInstance(api)
+
+router.beforeEach((to, from, next) => {
+  routerAuthController(to, next)
+})
 
 app.mount('#app')
