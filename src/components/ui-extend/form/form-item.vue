6622ae31714f4110e59a225e3880783a6536ea7b
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-28 12:12:41 +0500

    Create form-item component

---
+++ src/components/ui-extend/form/form-item.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Icon from '@/components/ui/icon/icon.vue'
+import { setBasicUiProps } from '@/compositions/ui/setBasicUiProps.js'
+
+const props = defineProps({
+  error: {
+    type: String,
+    default: ''
+  }
+})
+
+const { classes } = setBasicUiProps(props, 'form-item-component')
+</script>
+
+<template>
+  <div class="flex flex-col w-full">
+    <slot />
+    <div v-if="error" class="text-xs mt-1">
+      {{ error }}
+    </div>
+  </div>
+</template>
