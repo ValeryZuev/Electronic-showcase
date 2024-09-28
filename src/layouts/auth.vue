6216795b6f24422c4c9083723ef8d62e55fbcb5d
ValeryZuev <51708691+ValeryZuev@users.noreply.github.com>
2024-09-28 12:13:56 +0500

    Create auth layout

---
+++ src/layouts/auth.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="container flex flex-col gap-4 pt-4 flex-1">
+    <main class="flex flex-col items-center justify-center">
+      <RouterView />
+    </main>
+  </div>
+</template>
